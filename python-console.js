261c54266b2aa4bddbb45c2efefb3bd7bd63b48a
GabrieleMaurina <gabrielemaurina95@gmail.com>
2018-03-29 18:07:48 +0200

    Added file execution. Added test file. Handled errors.

--- python-console.js
+++ python-console.js
@@ -1,18 +1,26 @@
+var args = Array.from(process.argv).slice(2)
+args = args.length > 0 ? args : ['process.py']
+
 const readline = require('readline').createInterface(process.stdin, process.stdout)
-const proc = require('child_process').spawn('python', ['process.py'], ['pipe', 'pipe','pipe'])
+const proc = require('child_process').spawn('python', args, ['pipe', 'pipe','pipe'])
 
 proc.stdout.on('data', (data) => {
-  process.stdout.write(data.toString())
+	process.stdout.write(data.toString())
 })
 
 proc.stderr.on('data', (data) => {
-  process.stdout.write(data.toString())
+	process.stdout.write(data.toString())
 })
 
 proc.on('exit', () => {
-  process.exit()
+	process.exit()
+})
+
+proc.on('error', (err) => {
+	console.log(err)
+	process.exit()
 })
 
 readline.on('line', (data) => {
-  proc.stdin.write(data + '\n')
+	proc.stdin.write(data + '\n')
 })
---
+++ test.py
@@ -0,0 +1,5 @@
+import sys
+
+print('a')
+name = input('name? ')
+print('hello ' + name + ' ' + sys.argv[1])
